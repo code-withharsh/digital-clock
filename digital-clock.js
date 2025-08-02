e03933b59752dc7476384295f8a81f035ebd87fe
code-withharsh <harshbhardwaj2604@gmail.com>
2025-08-02 11:51:41 +0530

    Add files via upload
    added code

---
+++ digital-clock.js
@@ -0,0 +1,31 @@
+const hourE1 = document.getElementById('hours');
+const minuteE1 = document.getElementById('minutes');
+const seconE1 = document.getElementById('seconds');
+const ampmE1 = document.getElementById('ampm');
+
+function updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "Am";
+    if (h > 12){
+        h = h-12;
+        ampm = "Pm";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    
+    hourE1.innerText = h;
+    minuteE1.innerText = m;
+    seconE1.innerText = s;
+    ampmE1.innerText = ampm ;
+
+    setTimeout(() => {
+        updateClock();
+
+    }, 1000);
+}
+
+updateClock();
